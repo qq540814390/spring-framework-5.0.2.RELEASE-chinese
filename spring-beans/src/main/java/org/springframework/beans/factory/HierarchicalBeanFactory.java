692b5f575c8e5afc637f62b6e97dae572c5c3ac3
yooga0012 <339239023@qq.com>
2020-01-19 10:57:04 +0800

    init

---
+++ spring-beans/src/main/java/org/springframework/beans/factory/HierarchicalBeanFactory.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2002-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.beans.factory;
+
+import org.springframework.lang.Nullable;
+
+/**
+ * Sub-interface implemented by bean factories that can be part
+ * of a hierarchy.
+ *
+ * <p>The corresponding {@code setParentBeanFactory} method for bean
+ * factories that allow setting the parent in a configurable
+ * fashion can be found in the ConfigurableBeanFactory interface.
+ *
+ * @author Rod Johnson
+ * @author Juergen Hoeller
+ * @since 07.07.2003
+ * @see org.springframework.beans.factory.config.ConfigurableBeanFactory#setParentBeanFactory
+ */
+//层次性的Hierarchical，类似于ClassLoader双亲委派
+//ClassLoader双亲委派：某个特定的类加载器在接到加载类的请求时，首先将加载任务委托给父类加载器，
+//依次递归，如果父类加载器可以完成类加载任务，就成功返回；只有父类加载器无法完成此加载任务时，才自己去加载
+public interface HierarchicalBeanFactory extends BeanFactory {
+
+	/**
+	 * Return the parent bean factory, or {@code null} if there is none.
+	 */
+	@Nullable
+	BeanFactory getParentBeanFactory();
+
+	/**
+	 * Return whether the local bean factory contains a bean of the given name,
+	 * ignoring beans defined in ancestor contexts.
+	 * <p>This is an alternative to {@code containsBean}, ignoring a bean
+	 * of the given name from an ancestor bean factory.
+	 * @param name the name of the bean to query
+	 * @return whether a bean with the given name is defined in the local factory
+	 * @see BeanFactory#containsBean
+	 */
+	boolean containsLocalBean(String name);
+
+}
